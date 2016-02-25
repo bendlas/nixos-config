51d3528fd899a62ee95c15e0f3d00defd53f1600
Herwig Hochleitner <herwig@bendlas.net>
2016-02-25 04:41:59 +0100

    deactivated go for build failures

--- desktop.nix
+++ desktop.nix
@@ -5,17 +5,17 @@
 
     pciutils ntfs3g wireshark xpra roxterm nodejs unetbootin gparted
 
-    debootstrap mercurial subversion cmake rustc cargo nim ant pypy pixie
-    pythonPackages.ipython guile guile_lib nodePackages.grunt-cli lua go mono
+    debootstrap mercurial subversion cmake rustc cargo nim ant pixie pypy # go
+    pythonPackages.ipython guile guile_lib nodePackages.grunt-cli lua mono
     luajit luarocks androidsdk_4_4 racket # sdlmame ## ftp.archlinux.org unavailable
 
     dmenu glxinfo liberation_ttf xlibs.xkill xlibs.xmodmap
     xlibs.xbacklight xlibs.xrandr xlibs.xev xlibs.xkbcomp aspell
-    aspellDicts.en aspellDicts.de dunst libnotify google-musicmanager
+    aspellDicts.en aspellDicts.de dunst libnotify
 
     firefoxWrapper deluge dosbox alsaUtils clementine gimp geoip
     idea.idea-ultimate chromium vlc inkscape steam dropbox-cli bitcoin
-    unrar p7zip gitAndTools.hub bsdiff antimony blender
+    unrar p7zip bsdiff antimony blender # gitAndTools.hub
     links2 texLiveFull wine winetricks qjackctl jack2Full beep radare2
     valgrind sbcl npm2nix lyx nix-generate-from-cpan paprefs pavucontrol
     pinentry pgadmin audacity dos2unix
