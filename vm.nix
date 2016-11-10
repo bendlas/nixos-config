e01b6b48562db29083e0e2811ed8db10fb902d56
Herwig Hochleitner <herwig@bendlas.net>
2016-11-10 05:07:24 +0100

    vm config for x tests

--- vm.nix
+++ vm.nix
@@ -1,20 +1,20 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
   imports = [
     #./base.nix
     ./desktop.nix # ./xsession.nix
   ];
 
-  users.extraUsers.herwig.password = "123";
-  users.extraUsers.herwig.isNormalUser = true;
+  users.extraUsers.root.password = "123";
+  users.extraUsers.vm.password = "123";
+  users.extraUsers.vm.isNormalUser = true;
   users.mutableUsers = false;
 
-  networking = rec {
-    hostName = "vm";
-    hostId = "e26c37cc";
-  };
+  networking.hostName = "vm";
 
-  environment.systemPackages = with pkgs; [
-  ];
+  environment.systemPackages = with pkgs; [ ];
+  environment.enableDebugInfo = true;
 
+  services.xserver.displayManager.qingy.enable = true;
+  services.xserver.displayManager.kdm.enable = lib.mkForce false;
 }
