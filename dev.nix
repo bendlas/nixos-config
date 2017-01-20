9060d9334abb93bc8fdc3c6d212f5182c75f6f8b
Herwig Hochleitner <herwig@bendlas.net>
2017-01-20 13:16:51 +0100

    add 8020 to forwarded ports for dev

--- dev.nix
+++ dev.nix
@@ -9,7 +9,7 @@
       127.0.0.1 leihfix.local static.local jk.local hdnews.local hdirect.local stats.local
     '';
     firewall = {
-      allowedTCPPorts = [ 22 8023 8580 8581 8582 8080 ]; # 80 443 3449 8000 8020
+      allowedTCPPorts = [ 22 8020 8023 8580 8581 8582 8080 ]; # 80 443 3449 8000
       allowPing = true;
     };
     nat = {
