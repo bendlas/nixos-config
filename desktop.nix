4a51690e5a6ffe00e5a4e14fd9505810a2c73b00
Herwig Hochleitner <herwig@bendlas.net>
2018-07-31 12:54:27 +0200

    disable blender build

--- desktop.nix
+++ desktop.nix
@@ -17,7 +17,7 @@
 
     firefox deluge dosbox alsaUtils gimp geoip # clementine
     chromium vlc inkscape dropbox-cli steam idea.idea-community bitcoin
-    unrar p7zip bsdiff gitAndTools.hub blender # antimony
+    unrar p7zip bsdiff gitAndTools.hub # blender # antimony
     links2 qjackctl jack2Full beep wine winetricks radare2
     valgrind sbcl lyx nix-generate-from-cpan paprefs pavucontrol npm2nix
     pinentry dos2unix audacity pgadmin
