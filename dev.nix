68b864bd8a5fa453369b2f657074a63681efbe4e
Herwig Hochleitner <herwig@bendlas.net>
2019-02-24 23:49:47 +0100

    port for audiolooper

--- dev.nix
+++ dev.nix
@@ -16,7 +16,7 @@ in
     '';
     firewall = {
       #allowedTCPPorts = [ 22 ];
-      allowedTCPPorts = [ 22 80 443 3449 8081 8000 8080 9981 9982 ];
+      allowedTCPPorts = [ 22 80 443 3449 8081 8000 8050 8080 9981 9982 ];
       allowedUDPPorts = [ 9981 9982 ];
       #allowedTCPPortRanges = [
       #  { from = 8000; to = 9000; }
