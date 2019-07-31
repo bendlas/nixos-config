e893a9a26f4b21c88ac05c4c3ff6720093268285
Herwig Hochleitner <herwig@bendlas.net>
2019-07-31 16:05:23 +0200

    zfs on nitox

--- nitox.nix
+++ nitox.nix
@@ -28,6 +28,7 @@
     };
     kernelParams = [ "nomodeset" "resume=UUID=58a029ec-27e3-49cd-9ec1-2452ede1cec5" "resume=UUID=c4bd389b-dd2d-4777-a2f3-d55bbe000566" ];
     initrd.availableKernelModules = [ "bcache" ];
+    supportedFilesystems = [ "zfs" ];
     extraModprobeConfig = ''
       options libahci             skip_host_reset=1
     '';
