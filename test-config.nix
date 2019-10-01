b35cf03b1e9af1252a5a792181b3a0f57fac2129
Herwig Hochleitner <herwig@bendlas.net>
2019-10-01 15:12:06 +0200

    fix typo

--- test-config.nix
+++ test-config.nix
@@ -3,7 +3,7 @@
   imports = [ ./desktop.nix ./dev.nix ];
   fileSystems."/" = { device = "/dev/null"; };
   boot.loader.grub.enable = false;
-  boot.supportedFileSystems = [ "zfs" ];
+  boot.supportedFilesystems = [ "zfs" ];
   networking.hostId = "cafebabe";
   networking.nat.externalInterface = "dummy";
   services.xserver.videoDrivers = [ "nvidia" "nouveau" "nv" "vesa" "intel" ];
