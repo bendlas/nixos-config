30cce58a00364aedb8746387cb02b8b0c039e561
Herwig Hochleitner <herwig@bendlas.net>
2019-10-07 17:02:59 +0200

    disallow ssh keyboard-interactive

--- base.nix
+++ base.nix
@@ -104,6 +104,7 @@ in {
     openssh = {
       enable = true;
       passwordAuthentication = false;
+      challengeResponseAuthentication = false;
       startWhenNeeded = true;
     };
     locate.enable = false;
