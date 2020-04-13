5eb1ed103320afb1b5b50e697ee80a5d4265b23d
Herwig Hochleitner <herwig@bendlas.net>
2020-04-13 12:35:56 +0200

    extend taalo commands

--- taalo-build.nix
+++ taalo-build.nix
@@ -9,11 +9,25 @@ let
     exec ${lib.escapeShellArg nix}/bin/${cmd} "$@"
   '';
 
+  downloadScript = lib.escapeShellArg ''
+    #!${stdenv.shell}
+    exec ${lib.escapeShellArg nix}/bin/nix-build --option binary-caches "https://cache.nixos.org/ https://headcounter.org/hydra" "$@"
+  '';
+
+  bdScript = lib.escapeShellArg ''
+    #!${stdenv.shell}
+    taalo-build "$@"
+    exec taalo-download "$@"
+  '';
+
 in runCommand "taalo-build" {} ''
   mkdir -p "$out/bin"
 
   echo -n ${mkScript "nix-build"} > "$out/bin/taalo-build"
   echo -n ${mkScript "nix-store -r"} > "$out/bin/taalo-realize"
 
-  chmod +x "$out"/bin/taalo-{build,realize}
+  echo -n ${downloadScript} > "$out/bin/taalo-download"
+  echo -n ${bdScript} > "$out/bin/taalo-bd"
+
+  chmod +x "$out"/bin/taalo-{build,realize,download,bd}
 ''
