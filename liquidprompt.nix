d3e151c219fc98d7b4eddd8f14a99e110c828b09
Herwig Hochleitner <herwig@bendlas.net>
2021-11-23 18:17:13 +0100

    updates

--- liquidprompt.nix
+++ liquidprompt.nix
@@ -4,5 +4,5 @@ fetchFromGitHub {
   owner = "nojhan";
   repo = "liquidprompt";
   rev = "v2.0.3";
-  sha256 = "11rcn19jj9dkj4nly7zipmfxbv0g7zi5l8wzgfgdmggbagnqf02h";
+  sha256 = "sha256-BY0/lcBxcfIgKXbXZjiEgVdugmo8vD6UWGsjEVq6j6E=";
 }
