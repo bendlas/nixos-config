0ce1d569cc097634134b086b053b4dbb94345ca4
Natox Local <herwig@natox.local>
2022-01-26 00:28:52 +0100

    epson printer driver

--- epson-stylus-photo-r3000.module.nix
+++ epson-stylus-photo-r3000.module.nix
@@ -2,7 +2,7 @@
 let
   pname = "epson-stylus-photo-r3000";
   version = "1.0.0-1lsb3.2_amd64";
-  pkg = pkgs.runCommand "dpkg-unpack" {
+  pkgFor = pkgs: pkgs.runCommand "dpkg-unpack" {
     inherit version pname;
     src = pkgs.fetchurl {
       url = "http://download.ebz.epson.net/dsc/op/stable/debian/dists/lsb3.2/main/binary-amd64/epson-inkjet-printer-stylus-photo-r3000_${version}.deb";
@@ -12,12 +12,40 @@ let
   } ''
     dpkg -x $src $out
   '';
+  # trampoline = pkgs.writeScript "trampoline" ''
+  #     #!/bin/sh
+  #     exec "$@"
+  #   ''
+  filterProgram = pkgs.buildFHSUserEnvBubblewrap rec {
+    name = "${pname}${version}";
+    runScript = "/opt/epson-inkjet-printer-stylus-photo-r3000/cups/lib/filter/${passthru.binName}";
+    targetPkgs = pkgs: [
+      (pkgFor pkgs)
+      pkgs.libjpeg
+      pkgs.cups
+    ];
+    extraOutputsToInstall = [ "opt" ];
+    extraBuildCommands = ''
+      (cd lib; ln -s ./ld-2.33.so ld-lsb-x86-64.so.3)
+    '';
+    passthru.ppdName = "Epson-Stylus_Photo_R3000-epson-driver.ppd";
+    passthru.binName = "epson_inkjet_printer_filter";
+    passthru.ppdz = "${pkgFor pkgs}/opt/epson-inkjet-printer-stylus-photo-r3000/ppds/Epson/${passthru.ppdName}.gz";
+  };
 in
 with pkgs.lib;
 with types;
 {
 
   options.bendlas.pkgs."${pname}" = mkOption { type = package; };
-  config.bendlas.pkgs."${pname}" = pkg;
+  config.bendlas.pkgs."${pname}" = pkgs.runCommand "epson-driver" {
+  }''
+    mkdir -p $out/lib/cups/filter $out/share/cups/model/Epson
+    ln -s ${filterProgram} $out/lib/cups/filter/${filterProgram.binName}
+    zcat ${filterProgram.ppdz} \
+      | sed "s#/opt/epson-inkjet-printer-stylus-photo-r3000/cups/lib/filter/epson_inkjet_printer_filter#${filterProgram}#" \
+      > $out/share/cups/model/Epson/${filterProgram.ppdName}
+  '';
+  config.services.printing.drivers = [ config.bendlas.pkgs."${pname}" ];
 
 }
