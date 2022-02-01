a2816583748ef0e396b895458b47cb7288069ce9
Rastox Local <herwig@nitox.local>
2022-02-01 02:35:25 +0100

    rastox: disable gnome-help

--- rastox/configuration.nix
+++ rastox/configuration.nix
@@ -20,7 +20,9 @@ in {
       # ./video-rpi.nix
     ];
 
+  ## disable man and gnome-help
   documentation.enable = false;
+  environment.gnome.excludePackages = [ pkgs.gnome.yelp ];
 
   boot = {
     consoleLogLevel = 7;
