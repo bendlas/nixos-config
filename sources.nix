aab46340c78885f864e72a5e277cd42b02163177
Pinox Local <nixos@pinox.local>
2022-02-01 16:29:48 +0100

    prevent config shell from being GCd

--- sources.nix
+++ sources.nix
@@ -2,15 +2,20 @@
 
 with lib;
 let
-  inherit (pkgs.callPackage ./config-shell.nix {
+  config-shell = pkgs.callPackage ./config-shell.nix {
     inherit (config.bendlas) machine;
-  }) nixpkgs configs mobile-nixos nixos-hardware machine;
+  };
+  inherit (config-shell) nixpkgs nixpkgs-stable nixpkgs-unstable
+    configs mobile-nixos nixos-hardware machine;
 in {
   imports = [ ./name.module.nix ];
   # keep synchronized with ./config-shell.nix
   config = {
+    system.extraDependencies = [ config-shell ];
     environment.etc."nixos".source = configs;
     environment.etc."nixpkgs".source = nixpkgs;
+    environment.etc."nixpkgs-unstable".source = nixpkgs-unstable;
+    environment.etc."nixpkgs-stable".source = nixpkgs-stable;
     environment.etc."mobile-nixos".source = mobile-nixos;
     environment.etc."nixos-hardware".source = nixos-hardware;
     ## this is set non-configurable in <nixos/modules/programs/environment.nix>
