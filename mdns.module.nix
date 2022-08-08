e2c009deb0b8f4f3b0ee3d97de18b521f15f79a9
Natox Local <herwig@natox.local>
2022-08-08 18:48:57 +0200

    avahi: cups fix

--- mdns.module.nix
+++ mdns.module.nix
@@ -1,13 +1,26 @@
 { lib, config, ... }:
 {
 
-  require = [ ./name.module.nix ];
+  require = [
+    ./name.module.nix
+    ({ pkgs, config, ... }: {
+      ## fix CUPS issues
+      ## see https://github.com/NixOS/nixpkgs/issues/118628
+      services.avahi.nssmdns = pkgs.lib.mkForce false; # Use my settings from below
+      # settings from avahi-daemon.nix where mdns is replaced with mdns4
+      system.nssModules = with pkgs.lib; optional (!config.services.avahi.nssmdns) pkgs.nssmdns;
+      system.nssDatabases.hosts = with pkgs.lib; optionals (!config.services.avahi.nssmdns) (mkMerge [
+        (mkOrder 900 [ "mdns4_minimal [NOTFOUND=return]" ]) # must be before resolve
+        (mkOrder 1501 [ "mdns4" ]) # 1501 to ensure it's after dns
+      ]);
+    })
+  ];
 
   services.avahi = {
     enable = true;
     publish.enable = true;
     publish.addresses = true;
-    nssmdns = true;
+    nssmdns = true; ## overridde for now, see require above
     wideArea = false;
     # extraServiceFiles.ssh = "${pkgs.avahi}/etc/avahi/services/ssh.service";
     interfaces = lib.mkDefault (throw "Please set `services.avahi.interfaces` explicitly, in order to avoid configuring ve-* interfaces");
