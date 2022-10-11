894ce1796a7dfa3742762156480b9b2f45d94726
Herwig Hochleitner <herwig@bendlas.net>
2022-10-11 04:00:26 +0200

    pinox: add waydroid

--- pinox.nix
+++ pinox.nix
@@ -7,7 +7,7 @@
     ./log.module.nix ./sources.module.nix ./nix.module.nix ./zsh.module.nix
     ./locale.module.nix ./ssh.module.nix ./essential.module.nix ./mdns.module.nix
     # new base
-    ./access.module.nix ./tmpfs.module.nix ./docu-disable.module.nix ./nm-iwd.module.nix
+    ./access.module.nix ./tmpfs.module.nix ./docu-disable.module.nix ./nm-iwd.module.nix ./waydroid.module.nix
     # shared with ./desktop.nix
     ./sound.module.nix
     # mobile-nixos
