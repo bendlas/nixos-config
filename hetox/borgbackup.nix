c78b0a58f0935587800f975a0f697ef12ddb53e0
Herwig Hochleitner <herwig@bendlas.net>
2022-12-20 21:41:17 +0100

    hetox: add herwig repo to borgbackup

--- hetox/borgbackup.nix
+++ hetox/borgbackup.nix
@@ -5,7 +5,17 @@
       authorizedKeysAppendOnly = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKKsTTvCNGI1NDr25uh7/neFy9aED5g6xic0M/RA+EFe valheim@contox"
       ];
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfZYoqHjtHqoJPk3ZhDVX6w/Tg49FqF5XTaOWRqYtSq herwig@lenix"
+      ];
       path = "/var/borgbackup/valheim-contox";
+    };
+    herwig = {
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfZYoqHjtHqoJPk3ZhDVX6w/Tg49FqF5XTaOWRqYtSq herwig@lenix"
+      ];
+      path = "/var/borgbackup/herwig";
+      allowSubRepos = true;
     };
   };
 
