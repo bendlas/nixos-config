c73676e0355694c5ee55cbfb680312f9cd616987
Herwig Hochleitner <herwig@bendlas.net>
2022-12-20 19:32:08 +0100

    move hetox main config into toplevel

--- hetox.nix
+++ hetox.nix
@@ -1,16 +1,34 @@
+{ pkgs, ... }:
 {
+
   bendlas.machine = "hetox";
   imports = [
+    ./hetox/hardware-configuration.nix ./hetox/gitlab.nix
     # shared with ./base.nix
     ./log.module.nix ./sources.module.nix ./nix.module.nix ./zsh.module.nix
     ./locale.module.nix ./ssh.module.nix ./essential.module.nix ./convenient.module.nix
     # new base
     ./access.module.nix ./tmpfs.module.nix ./nginx.module.nix
-    # gitlab / hdnews server
-    ./hetox/configuration.nix
     ## TODO
     # ./nextcloud.module.nix
     # ./matrix-server.module.nix
   ];
+
   users.users.herwig.isNormalUser = true;
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.devices = [ "/dev/sda" ];
+  networking.hostName = "hetox"; # Define your hostname.
+  networking.useDHCP = false;
+  networking.interfaces.ens3.useDHCP = true;
+  networking.nat.externalInterface = "ens3";
+
+  environment.systemPackages = with pkgs; [
+    # emacsBendlasNox
+    emacs-nox
+  ];
+
+  system.stateVersion = "21.05"; # Did you read the comment?
+
 }
