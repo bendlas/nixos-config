fa893e0d77da19eff0c632c0bb797b850954211c
Rastox Local <herwig@nitox.local>
2023-01-04 04:01:09 +0100

    rastox: disable wifi

--- rastox/configuration.nix
+++ rastox/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { lib, config, pkgs, ... }:
 let
   cma = 512;
@@ -122,15 +118,13 @@ in {
   #   })
   # ];
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+  ## The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  ## Per-interface useDHCP will be mandatory in the future, so this generated config
+  ## replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
-  networking.interfaces.wlan0.useDHCP = true;
+  # networking.interfaces.wlan0.useDHCP = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     libraspberrypi raspberrypi-eeprom iw
     (pkgs.runCommand "custom-tools" {
@@ -144,12 +138,6 @@ in {
     '')
   ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
+  system.stateVersion = "21.11";
 
 }
