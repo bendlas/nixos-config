804868063399f0103a6080d8a346c448c5efa4d7
Herwig Hochleitner <herwig@bendlas.net>
2023-01-04 04:26:57 +0100

    hetox: fix borgbackup server

--- hetox/borgbackup.nix
+++ hetox/borgbackup.nix
@@ -5,14 +5,11 @@
       authorizedKeysAppendOnly = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKKsTTvCNGI1NDr25uh7/neFy9aED5g6xic0M/RA+EFe valheim@contox"
       ];
-      authorizedKeys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfZYoqHjtHqoJPk3ZhDVX6w/Tg49FqF5XTaOWRqYtSq herwig@lenix"
-      ];
       path = "/var/borgbackup/valheim-contox";
     };
     herwig = {
-      authorizedKeys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICfZYoqHjtHqoJPk3ZhDVX6w/Tg49FqF5XTaOWRqYtSq herwig@lenix"
+      authorizedKeysAppendOnly = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/nw+URWOaWzJ3ZmT1BA2lKxGV0VyrOh9IbKF229kPw herwig@lenix"
       ];
       path = "/var/borgbackup/herwig";
       allowSubRepos = true;
